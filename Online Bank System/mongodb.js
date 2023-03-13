3dae3e6e6c9f7d5910dda5c552367a0d847e1db0
Kushal Gajjar <kushalgajjar1@hp.com>
2023-03-13 23:12:33 +0530

    Login and register function created

---
+++ Online Bank System/mongodb.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+try {
+    mongoose.connect("mongodb+srv://admin:admin@ffsdc16.pklkjft.mongodb.net/TEST?retryWrites=true&w=majority")
+}
+catch(e) {
+    console.log(e)
+}
+
+const LogInSchema = new mongoose.Schema({
+    firstName:{
+        type : String,
+        required : true
+    },
+    lastName:{
+        type : String,
+        required : true
+    },
+    eMail:{
+        type : String,
+        required : true
+    },
+    password:{
+        type : String,
+        required : true
+    }
+})
+
+const collection = new mongoose.model("Collection_1", LogInSchema);
+module.exports = collection;
--- Online Bank System/server.js
+++ Online Bank System/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const server = express();
 const bodyParse = require("body-parser");
+const collection = require("./mongodb")
 
 const port = 3000;
 const projectName = "MyBank";
@@ -9,22 +10,55 @@ server.use(bodyParse.urlencoded({extended: true}));
 server.use(express.static("assets"));
 server.set("view engine", "ejs");
 
-server.listen(port, function () {
-    console.log("Server is running on port " + port + ".");
-});
-
 server.get("/", function (req, res) {
     res.render("home_page", {projectName: projectName});
 });
 
 server.get("/login", function (req, res) {
-    res.render("login", {projectName: projectName + " - Login"});
+    res.render("login", {projectName: projectName});
 });
 
 server.get("/registration", function (req, res) {
-    res.render("registration", {projectName: projectName + " - Register"});
+    res.render("registration", {projectName: projectName});
 });
 
 server.get("/about", function (req, res) {
-    res.render("about_us", {projectName: projectName + " - About"});
+    res.render("about_us", {projectName: projectName});
+});
+
+server.get("/main", function (req, res){
+    res.render("main", {projectName: projectName})
+})
+
+server.post("/register", async(req, res)=>{
+    if(await collection.findOne({eMail: req.body.eMail})){
+        console.log(req.body);
+        res.status(400).send({
+            message: "Email already exists"
+        })
+        return
+    }
+    await new collection(req.body).save()
+    res.redirect("/main")
+    res.render("main", {projectName: projectName})
+})
+
+server.post("/login", async(req, res)=>{
+    try{
+        const check = await collection.findOne({name:req.body.name})
+        if(check.password===req.body.password){
+            res.redirect("/main")
+            res.render("main", {projectName: projectName})
+        }
+        else{
+            res.send("wrong password")
+        }
+    }
+    catch{
+        res.send("wrong details")
+    }
+})
+
+server.listen(port, function () {
+    console.log("Server is running on port " + port + ".");
 });
