96d94c089522729bbf964b914ff0583ca5170828
Mehul Agarwal <mehul@lenovo.com>
2023-03-14 17:21:38 +0530

    prettied

--- Online Bank System/app.js
+++ Online Bank System/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParse = require("body-parser");
-const collection = require("./mongodb")
+const collection = require("./mongodb");
 
 const port = 3000;
 const projectName = "MyBank";
@@ -29,46 +29,41 @@ app.get("/about", function (req, res) {
     res.render("about_us", {projectName: projectName});
 });
 
-app.get("/main", function (req, res){
-    if(isLogged){
+app.get("/main", function (req, res) {
+    if (isLogged) {
         res.render("main", {projectName: projectName})
-    }
-    else{
+    } else {
         res.redirect("/login")
     }
 })
 
-app.post("/register", async(req, res)=>{
-    if(await collection.findOne({eMail: req.body.eMail})){
+app.post("/register", async (req, res) => {
+    if (await collection.findOne({eMail: req.body.eMail})) {
         console.log(req.body);
         res.status(400).send({
             message: "Email already exists"
-        })
-        return
-    }
-    else{
-        await new collection(req.body).save()
-        isLogged = true
-        res.redirect("/main")
+        });
+    } else {
+        await new collection(req.body).save();
+        isLogged = true;
+        res.redirect("/main");
     }
 })
 
-app.post("/login", async(req, res)=>{
-    try{
-        const check = await collection.findOne({name:req.body.name})
-        if(check.password===req.body.password){
+app.post("/login", async (req, res) => {
+    try {
+        const check = await collection.findOne({name: req.body.name})
+        if (check.password === req.body.password) {
             isLogged = true;
             res.redirect("/main")
-        }
-        else{
+        } else {
             res.send("wrong password")
         }
-    }
-    catch{
+    } catch {
         res.send("wrong details")
     }
 })
 
 app.listen(port, function () {
-    console.log("app is running on port " + port + ".");
+    console.log("Server is running on port " + port + ".");
 });
