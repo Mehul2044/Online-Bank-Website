1ca6633264a2b0b9b9b29f307779c160a9f26a32
Mehul Agarwal <mehul@lenovo.com>
2023-03-21 02:18:58 +0530

    add pages

--- Online Bank System/app.js
+++ Online Bank System/app.js
@@ -14,6 +14,8 @@ relation.createTables(db);
 let isLogged = false;
 let userName;
 
+let account_number;
+
 app.use(bodyParse.urlencoded({extended: true}));
 app.use(express.static("assets"));
 app.set("view engine", "ejs");
@@ -61,8 +63,24 @@ app.get("/main/transfer", function (req, res) {
     }
 });
 
+app.get("/main/update_profile", function (req, res) {
+    if (isLogged) {
+        res.render("update_profile", {projectName: projectName, userName: userName});
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/main/delete_account", function (req, res) {
+    if (isLogged) {
+        res.render("delete_account", {projectName: projectName, userName: userName});
+    } else {
+        res.redirect("/login");
+    }
+});
+
 app.post("/login", async (req, res) => {
-    let account_number = req.body.account_number;
+    account_number = req.body.account_number;
     let password = req.body.password;
     await db.get("SELECT password, fname FROM accounts WHERE account_number = ?", [account_number], (err, row) => {
         if (err) {
@@ -85,12 +103,11 @@ app.post("/login", async (req, res) => {
     });
 });
 
-app.post("/register", async (req, res) => {
+app.post("/registration", async (req, res) => {
     let firstName = req.body.firstName
     let lastName = req.body.lastName
     let eMail = req.body.eMail
     let password = req.body.password
-
     let exist;
 
     function checkAccountExists(eMail) {
@@ -121,6 +138,17 @@ app.post("/register", async (req, res) => {
         });
         userName = firstName;
     }
+});
+
+app.post("/main/delete_account", function (req, res) {
+    db.run("insert into delete_account_request values(?, ?, ?);", [account_number, req.body.reason, req.body.aadhar], (err) => {
+       if (err){
+           console.log(err.message);
+       }else {
+           console.log("Deletion request sent.");
+           res.redirect("/main");
+       }
+    });
 });
 
 app.listen(port, function () {
--- Online Bank System/models/sqlite.js
+++ Online Bank System/models/sqlite.js
@@ -17,4 +17,19 @@ exports.createTables = function (db) {
         } else {
         }
     });
+
+    db.run("create table if not exists delete_account_request\n" +
+        "(\n" +
+        "    acc_no integer      not null\n" +
+        "        constraint delete_account_request_accounts_account_number_fk\n" +
+        "            references accounts,\n" +
+        "    reason varchar(100) not null,\n" +
+        "    aadhaar_no varchar(20) not null\n" +
+        ");", err => {
+        if (err) {
+            console.log(err.message);
+        } else {
+
+        }
+    });
 }
